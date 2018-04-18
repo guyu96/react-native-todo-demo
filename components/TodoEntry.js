21ea7db82166768bad7fb079fc84bf20be143d4f
Guyu Fan <guyufan1996@gmail.com>
2018-04-18 20:42:11 +0400

    create todo app skeleton

---
+++ components/TodoEntry.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default class TodoEntry extends Component {
+  render() {
+
+  }
+}
---
+++ components/TodoList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, FlatList, View } from 'react-native';
+
+export default class TodoList extends Component {
+  render() {
+
+  }
+}
